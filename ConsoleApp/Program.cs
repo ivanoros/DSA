ce22540002c3d49d894df194b1f67e4b4d0c622b
Ivan Oros <ivan.oros@gmail.com>
2022-12-06 09:33:30 -0500

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using DSAClassLibrary.Recursion;
+
+// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+var recursion = new Recursion();
+recursion.HeadRecursion(3);
+Console.WriteLine("----------------");
+recursion.TailRecursion(3);
+//Console.ReadLine();
+var fun = recursion.RecursionWithStaticVariable(5);
+Console.WriteLine("----------------");
+Console.WriteLine(fun);
+var global = recursion.RecursionWithGloabVariable(5);
+Console.WriteLine("----------------");
+Console.WriteLine(global);
---
+++ DSAClassLibrary/Recursion/Recursion.cs
@@ -0,0 +1,45 @@
+﻿namespace DSAClassLibrary.Recursion
+{
+    public class Recursion
+    {
+        public void HeadRecursion(int n)
+        {
+            if (n > 0)
+            {
+                HeadRecursion(n - 1);
+                Console.WriteLine(n);
+            }
+        }
+
+        public void TailRecursion(int n)
+        {
+            if(n > 0)
+            {
+                Console.WriteLine(n);
+                TailRecursion(n - 1);
+            }
+        }
+
+        private static int x = 0;
+        public int RecursionWithStaticVariable(int n)
+        {
+            if(n > 0)
+            {
+                x++;
+                return RecursionWithStaticVariable(n - 1) + x;
+            }
+            return 0;
+        }
+
+        private int y = 0;
+        public int RecursionWithGloabVariable(int n)
+        {
+            if (n > 0)
+            {
+                y++;
+                return RecursionWithGloabVariable(n - 1) + y;
+            }
+            return 0;
+        }
+    }
+}
