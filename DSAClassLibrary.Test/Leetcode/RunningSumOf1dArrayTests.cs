642f46d7f1d7619d0b70fe0311e14d312fd8f907
Ivan Oros <ivan.oros@gmail.com>
2022-12-25 12:33:28 -0500

    Leetcode RunningSumOf1dArray

---
+++ DSAClassLibrary.Test/Leetcode/RunningSumOf1dArrayTests.cs
@@ -0,0 +1,44 @@
+﻿using DSAClassLibrary.Leetcode;
+
+namespace DSAClassLibrary.Test.Leetcode
+{
+    [TestClass]
+    public class RunningSumOf1dArrayTests
+    {
+        [TestMethod]
+        [DataRow(new int[] { 1, 2, 3, 4 }, new int[] { 1, 3, 6, 10 })]
+        [DataRow(new int[] { 1, 1, 1, 1, 1 }, new int[] { 1, 2, 3, 4, 5 })]
+        [DataRow(new int[] { 3, 1, 2, 10, 1 }, new int[] { 3, 4, 6, 16, 17 })]
+        public void RunningSumTest(int[] nums, int[] expected)
+        {
+            var sut = new RunningSumOf1dArray();
+            var actual = sut.RunningSum(nums);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow(new int[] { 1, 2, 3, 4 }, new int[] { 1, 3, 6, 10 })]
+        [DataRow(new int[] { 1, 1, 1, 1, 1 }, new int[] { 1, 2, 3, 4, 5 })]
+        [DataRow(new int[] { 3, 1, 2, 10, 1 }, new int[] { 3, 4, 6, 16, 17 })]
+        public void RunningSumNewTest(int[] nums, int[] expected)
+        {
+            var sut = new RunningSumOf1dArray();
+            var actual = sut.RunningSumNew(nums);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow(new int[] { 1, 2, 3, 4 }, new int[] { 1, 3, 6, 10 })]
+        [DataRow(new int[] { 1, 1, 1, 1, 1 }, new int[] { 1, 2, 3, 4, 5 })]
+        [DataRow(new int[] { 3, 1, 2, 10, 1 }, new int[] { 3, 4, 6, 16, 17 })]
+        public void RunningSumUsingSameInputArrayTest(int[] nums, int[] expected)
+        {
+            var sut = new RunningSumOf1dArray();
+            var actual = sut.RunningSumUsingSameInputArray(nums);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ DSAClassLibrary/Leetcode/RunningSumOf1dArray.cs
@@ -0,0 +1,53 @@
+﻿namespace DSAClassLibrary.Leetcode
+{
+    public class RunningSumOf1dArray
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            var runningSum = new int[nums.Length];
+            var previousSum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                runningSum[i] = previousSum + nums[i];
+                previousSum = runningSum[i];
+            }
+
+            return runningSum;
+        }
+
+        // Approach 1: Using Separate Space
+        // Complexity Analysis
+        // Time complexity: O(n) where nnn is the length of the input array.
+        // This is because we use a single loop that iterates over the entire array to calculate the running sum.
+        // Space complexity: O(1) since we don't use any additional space to find the running sum.
+        // Note that we do not take into consideration the space occupied by the output array.
+        public int[] RunningSumNew(int[] nums)
+        {
+            var runningSum = new int[nums.Length];
+            runningSum[0] = nums[0];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                runningSum[i] = runningSum[i - 1] + nums[i];
+            }
+
+            return runningSum;
+        }
+
+        // Approach 2: Using Input Array for Output
+        // Complexity Analysis
+        // Time complexity: O(n) where nnn is the length of input array.
+        // Space complexity: O(1) since we don't use any additional space to find the running sum.
+        // Note that we do not take into consideration the space occupied by the output array.
+
+        public int[] RunningSumUsingSameInputArray(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+            {
+                nums[i] = nums[i - 1] + nums[i];
+            }
+            return nums;
+        }
+    }
+}
